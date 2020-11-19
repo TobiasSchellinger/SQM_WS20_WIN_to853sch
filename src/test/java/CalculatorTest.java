9420776ebc77ecf1bfa2d944b1b88874a4ab4433
TobiasSchellinger <48826861+TobiasSchellinger@users.noreply.github.com>
2020-11-19 19:00:49 +0100

    Übung 3 - Bis Aufgabe 8 alles

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,32 +9,32 @@ public class CalculatorTest extends TestCase {
     }
 
     public void testAdd() {
-        assertEquals(100, calculator.add(90, 10));
-        assertEquals(-530, calculator.add(-600, 70));
-        assertEquals(0, calculator.add(2147483647, 1));
-        assertEquals( 0, calculator.add(-2147483648, -1));
+        assertEquals( 100, calculator.add(90, 10));
+        assertEquals( -530, calculator.add(-600, 70));
+        assertEquals( 0, calculator.add(Integer.MAX_VALUE, 1));
+        assertEquals( 0, calculator.add(Integer.MIN_VALUE, -1));
     }
 
     public void testSubtract() {
-        assertEquals(80, calculator.subtract(90, 10));
-        assertEquals(-670, calculator.subtract(-600, 70));
-        assertEquals(0, calculator.subtract(2147483647, -1));
-        assertEquals( 0, calculator.subtract(-2147483648, 1));
+        assertEquals( 80, calculator.subtract(90, 10));
+        assertEquals( -670, calculator.subtract(-600, 70));
+        assertEquals( 0, calculator.subtract(Integer.MAX_VALUE, -1));
+        assertEquals( 0, calculator.subtract(Integer.MIN_VALUE, 1));
     }
 
     public void testDuplicate() {
         assertEquals(900, calculator.duplicate(90, 10));
         assertEquals(-42000, calculator.duplicate(-600, 70));
-        assertEquals(0, calculator.duplicate(2147483647, 2));
-        assertEquals( 0, calculator.duplicate(-2147483648, 2));
+        assertEquals(0, calculator.duplicate(Integer.MAX_VALUE, 2));
+        assertEquals( 0, calculator.duplicate(Integer.MIN_VALUE, 2));
         assertEquals( 0, calculator.duplicate(1, 0));
     }
 
     public void testDivide() {
-        assertEquals(9, calculator.divide(90, 10));
-        assertEquals(-8, calculator.divide(-600, 70));
-        assertEquals(1073741823, calculator.divide(2147483647, 2));
-        assertEquals( -1073741824, calculator.divide(-2147483648, 2));
+        assertEquals( 9, calculator.divide(90, 10));
+        assertEquals( -8, calculator.divide(-600, 70));
+        assertEquals( -1073741823, calculator.divide(Integer.MAX_VALUE, -2));
+        assertEquals( 1073741824, calculator.divide(Integer.MIN_VALUE, -2));
         assertEquals( 0, calculator.divide(1, 0));
     }
 }
